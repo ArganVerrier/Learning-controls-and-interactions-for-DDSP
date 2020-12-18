2a4a2e820c403f83e57131a873f66902543a7d6d
ArganVerrier <51755010+ArganVerrier@users.noreply.github.com>
2020-12-18 19:08:17 +0100

    test commit

---
+++ classesModules.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 18 18:13:27 2020
+
+@author: argan
+"""
+import matplotlib.pyplot as plt
+plt.style.use('seabornCustom')
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.distributions as distrib
+import torchvision
+import torch.nn.functional as f
+
+
+import seaborn as sns
+
+import multiprocessing
+
+class AE(nn.Module):
+    def __init__(self, encoder, decoder, encoding_dim):
+        super(AE, self).__init__()
+        self.encoding_dims = encoding_dim
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+    
+class VAE(AE):
+    
+    def __init__(self, encoder, decoder, encoding_dims, latent_dims):
+        super(VAE, self).__init__(encoder, decoder, encoding_dims)
+        
+        self.latent_dims = latent_dims
+        
+        #self.mu = nn.Sequential(nn.Linear(self.encoding_dims, self.latent_dims), nn.ReLU())
+        self.linMu=nn.Linear(self.encoding_dims, self.latent_dims)
+        #self.sigma = nn.Sequential(nn.Linear(self.encoding_dims, self.latent_dims), nn.Softplus())
+        self.linSigma=nn.Linear(self.encoding_dims, self.latent_dims)
+        
+    def encode(self, x):
+        
+        x=self.encoder(x)
+        mu=f.relu(self.linMu(x))
+        sigma=f.softplus(self.linSigma(x))
+        # print("shape mu="+str(mu.shape))
+        # print("sigma mu="+str(sigma.shape))
+        
+        return mu, sigma
+    
+    def decode(self, z):
+        return self.decoder(z)
+
+    def forward(self, x):
+        # Encode the inputs
+        z_params = self.encode(x)
+        # Obtain latent samples and latent loss
+        z_tilde, kl_div = self.latent(x, z_params)
+        # Decode the samples
+        x_tilde = self.decode(z_tilde)
+        return x_tilde, kl_div
+    
+    def latent(self, x, z_params):
+        
+        n_batch=x.shape[0]
+        mu, sigma =z_params
+        #print(mu.shape)
+        
+        #reparamétrisation
+        q=distrib.Normal(torch.zeros(mu.shape), torch.ones(sigma.shape))
+        z=sigma*q.sample() + mu
+        #compute KL divergence
+        kl_div=0.5* torch.sum(1+sigma -torch.pow(mu, 2)- torch.exp(sigma))
+        kl_div=kl_div/n_batch
+        
+        #print("espace latent, dim = "+str(z.shape))
+        return z, kl_div
+    
+    
+class encoder(nn.Module):
+    def __init__(self, nin, n_latent = 16, n_hidden = 512, n_classes = 1):
+        super(encoder, self).__init__()
+        self.nin=nin
+        self.n_latent=n_latent
+        self.n_hidden=n_hidden
+        self.n_classes=n_classes
+        self.lin1 = nn.Linear(nin, n_hidden)
+        self.lin2 = nn.Linear(n_hidden, n_hidden)
+        
+    def forward(self, x):
+        x=x.view(-1, self.nin)
+        a1=f.relu(self.lin1(x))
+        a2=f.relu(self.lin2(a1))
+        a3=f.relu(self.lin2(a2))
+        
+        return a3
+    
+class decoder(nn.Module):
+    def __init__(self, nin, n_latent = 16, n_hidden = 512, n_classes = 1):
+        super(decoder, self).__init__()
+        self.nin=nin
+        self.n_latent=n_latent
+        self.n_hidden=n_hidden
+        self.n_classes=n_classes
+        self.lin1=nn.Linear(n_latent, n_hidden)
+        self.lin2=nn.Linear(n_hidden, n_hidden)
+        self.lin3=nn.Linear(n_hidden, nin)
+        
+    def forward(self, x):
+        x=x.view((-1, self.n_latent))
+        a1=f.relu(self.lin1(x))
+        a2=f.relu(self.lin2(a1))
+        a3=f.sigmoid(self.lin3(a2))
+        
+        return a3        
+
+def construct_encoder_decoder_modules(nin, n_latent = 16, n_hidden = 512, n_classes = 1):
+    encoder_module=encoder(nin=nin, n_latent=n_latent, n_hidden=n_hidden, n_classes=n_classes)
+    decoder_module=decoder(nin=nin, n_latent=n_latent, n_hidden=n_hidden, n_classes=n_classes)
+    
+    return encoder_module, decoder_module
+    
+    
+#%
+recons_criterion = torch.nn.MSELoss(reduction='mean')
+
+def compute_loss(model, x, batch_size):
+        
+    x_, kl_div=model.forward(x)
+    #x_.view(x.shape)
+    pxz=recons_criterion(x_.view(x.shape), x)
+    full_loss=torch.mean(torch.log(pxz))-kl_div
+    
+    
+    return full_loss, x_
+
+def train_step(model, x, optimizer, batch_size):
+    loss, x_=compute_loss(model, x, batch_size)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    return loss, x_
+
+def predIm(x, y, model):
+    coord=torch.tensor([x, y])
+    pred=model.decode(coord).detach().reshape((28,28))
+    return pred
+
---
+++ untitled5.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 17 19:31:18 2020
+
+@author: argan
+"""
+import matplotlib.pyplot as plt
+plt.style.use('seabornCustom')
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.distributions as distrib
+import torchvision
+
+from classesModules import *
+import seaborn as sns
+
+import multiprocessing
+
+
+if __name__ == "__main__":
+    #%Train/valid data set
+    dataset_dir = 'D:/Documents/Cours/M2/projet-ml/part1/data'
+    
+    # Going to use 80%/20% split for train/valid
+    valid_ratio = 0.2
+    
+    # Load the dataset for the training/validation sets
+    train_valid_dataset = torchvision.datasets.MNIST(root=dataset_dir, train=True, transform=torchvision.transforms.ToTensor(), download=True)
+    
+    # Split it into training and validation sets
+    nb_train = int((1.0 - valid_ratio) * len(train_valid_dataset))
+    nb_valid =  int(valid_ratio * len(train_valid_dataset))
+    
+    # Load the test set
+    train_dataset, valid_dataset = torch.utils.data.dataset.random_split(train_valid_dataset, [nb_train, nb_valid])
+    test_dataset = torchvision.datasets.MNIST(root=dataset_dir, transform=torchvision.transforms.ToTensor(),train=False)
+    
+    # Prepare 
+    num_threads = 4     # Loading the dataset is using 4 CPU threads
+    batch_size  = 128   # Using minibatches of 128 samples
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, num_workers=num_threads)
+    valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle=False, num_workers=num_threads)
+    torch.utils.data.DataLoader(dataset=test_dataset,batch_size=batch_size,shuffle=False,num_workers=num_threads)
+        
+    #% training
+    imRef, labRef=next(iter(train_loader))
+    
+    nClass=10
+    nHidden=512
+    nIn=imRef[1, 0, :, :].shape[0]*imRef[1, 0, :, :].shape[1]
+    nOut=nIn*nClass
+    
+    Nepoch=30
+    
+    encoder, decoder=construct_encoder_decoder_modules(nIn, n_latent=2, n_hidden=nHidden, n_classes=nClass)
+    
+    model=VAE(encoder, decoder, nHidden, 2)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    
+    losses=np.zeros((Nepoch,))
+
+
+
+
+   
+    for ind in range(Nepoch):
+        print(ind)
+        loss=0.
+        for i,(img, lab) in enumerate(train_loader):
+            lossStep, x_=train_step(model, img, optimizer, batch_size)
+            loss+=lossStep
+        losses[ind]=loss.mean().item()
+    
+#%%
+    x=np.linspace(-8., 8., 3)
+    y=np.linspace(-8.,8.,3)
+    fig=plt.figure()
+    for indx in range(x.size):
+        x_=x[indx]
+        for indy in range(y.size):
+            y_=y[indy]
+            ax=fig.add_subplot(3,3, indy+indx+1)
+            plt.imshow(predIm(x_, y_, model))
+            
+            
